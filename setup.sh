7cddf126dce17ba56305cd2204332b9a017db9b1
Thomas Montfort <61255722+tmonty12@users.noreply.github.com>
2025-01-02 14:18:40 -0800

    launchable fixes (#1)
    * fixes to launchable
    
    * exit setup script on error

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # Install uv if not already installed
 if ! command -v uv &> /dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
+
+# Add uv to path
+source $HOME/.local/bin/env
 
 # Create a new virtual environment
 uv venv
